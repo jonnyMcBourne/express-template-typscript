294d4c4bc90c0ba459c448ed55122216b7369190
srOscuro <sroscuro@srOscuros-MacBook-Pro.local>
2024-03-03 11:02:05 -0600

    hotfix path

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 require('dotenv').config();
-import {server as Server} from './models/Server';
+import {server as Server} from './src/models/Server';
 const server = new Server();
 
 server.listen();
